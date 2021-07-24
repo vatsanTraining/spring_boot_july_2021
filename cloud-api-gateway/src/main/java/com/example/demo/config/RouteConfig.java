1260bfe159d158f6a50e4e246c37cd003b7a6b07
srivatsan <vatsank@gmail.com>
2021-07-24 14:38:13 +0530

    RouteLocator and Route Config Added

---
+++ cloud-api-gateway/src/main/java/com/example/demo/config/RouteConfig.java
@@ -0,0 +1,20 @@
+package com.example.demo.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RouteConfig {
+
+	
+	@Bean
+	public RouteLocator gateWayRoutes(RouteLocatorBuilder builder) {
+		
+		 return builder.routes()
+		           .route(p -> p.path("/api/v1/customers/**")
+		                    .uri("lb://AGENT-SERVICE"))
+		                .build();		
+	}
+}
