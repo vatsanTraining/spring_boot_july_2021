7bfe5f4dee24c155dde7efbc8b0e3dc7444103fc
srivatsan <vatsank@gmail.com>
2021-07-25 11:10:12 +0530

    Update

--- service_b_enabled_with_circuitbreaker/src/main/java/com/example/demo/controllers/ClientController.java
+++ service_b_enabled_with_circuitbreaker/src/main/java/com/example/demo/controllers/ClientController.java
@@ -5,20 +5,31 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+
 @RestController
 public class ClientController {
 
 	
 	  @Autowired
 	  RestTemplate template;
-	  
+
+	
 	  
 	  @GetMapping(path = "/client")
+	  @CircuitBreaker(name = "infoService", fallbackMethod = "getInfoFallback")
 	  public String getInfo() {
 		  
 		  return this.template.getForObject("http://localhost:6060/info", String.class);
 	  }
 	  
 	  
+	  public String getInfoFallback(Exception e) {
+		  
+		  //	  return this.template.getForObject("http://localhost:6066/info", String.class);
+		  
+		  return "This is cached  message";
+
+	  }
 	  
 }
