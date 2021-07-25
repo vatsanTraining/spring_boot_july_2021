c69a24e8bde0880c53c798c8b78f3da4546a38d3
srivatsan <vatsank@gmail.com>
2021-07-25 16:29:18 +0530

    Update

--- customer-service/src/main/java/com/example/demo/controllers/CustomerController.java
+++ customer-service/src/main/java/com/example/demo/controllers/CustomerController.java
@@ -1,12 +1,13 @@
 package com.example.demo.controllers;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-
+@RefreshScope
 @RequestMapping(path = "/api/v1")
 public class CustomerController {
 
